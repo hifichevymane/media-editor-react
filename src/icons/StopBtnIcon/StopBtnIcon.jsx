014125118a20073076a56ed28b8d34810d67b9a2
hifichevymane <blekbjek@gmail.com>
2024-08-22 23:04:59 +0300

    install wavesurfer js. use it in the project

---
+++ src/icons/StopBtnIcon/StopBtnIcon.jsx
@@ -0,0 +1,8 @@
+export default function StopBtnIcon () {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+      <rect width={4} height={14} x={6} y={5} fill="currentColor" rx={1}></rect>
+      <rect width={4} height={14} x={14} y={5} fill="currentColor" rx={1}></rect>
+    </svg>
+  )
+}
